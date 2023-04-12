5e886faa4bf9f29737c1d678dbef3a8e57dad553
jaelps <jaelsonpsilva18@gmail.com>
2023-04-12 02:11:10 -0300

    ok

---
+++ script.js
@@ -0,0 +1,42 @@
+var bar1pAtiva = window.document.querySelector('.mark')
+var bar2pAtiva = window.document.querySelector('.angela')
+var bar3pAtiva = window.document.querySelector('.jacob')
+var bar4pAtiva = window.document.querySelector('.rizky')
+var bar5pAtiva = window.document.querySelector('.kimberly')
+var bar6pAtiva = window.document.querySelector('.nathan')
+var bar7pAtiva = window.document.querySelector('.anna')
+
+/*var bar1pAtiva = window.document.querySelector('.mark')
+    bar1pAtiva.style = 'background-color: var(--White)'
+
+    var bar2pAtiva = window.document.querySelector('.angela')
+    bar2pAtiva.style = 'background-color: var(--White)'
+
+    var bar3pAtiva = window.document.querySelector('.jacob')
+    bar3pAtiva.style = 'background-color: var(--White)'
+
+    var bar4pAtiva = window.document.querySelector('.rizky')
+    bar4pAtiva.style = 'background-color: var(--White)'
+
+    var bar5pAtiva = window.document.querySelector('.kimberly')
+    bar5pAtiva.style = 'background-color: var(--White)'
+
+    var bar6pAtiva = window.document.querySelector('.nathan')
+    bar6pAtiva.style = 'background-color: var(--White)'
+
+    var bar7pAtiva = window.document.querySelector('.anna')
+    bar7pAtiva.style = 'background-color: var(--White)'*/
+
+bar1pAtiva.addEventListener("click",()=>{
+    bar1pAtiva.style = 'background-color: var(--White)'
+})
+
+bar2pAtiva.addEventListener("click",()=>{
+    bar2pAtiva.style = 'background-color: var(--White)'
+})
+
+bar3pAtiva.addEventListener("click",()=>{
+    bar3pAtiva.style = 'background-color: var(--White)'
+})
+
+
